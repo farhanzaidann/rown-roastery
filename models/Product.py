36b7befd310d0d66972a915df5117182cc0e9940
Sahroell <sahrulbaihaqiasidiq7@gmail.com>
2025-11-06 14:05:58 +0700

    sahrul: set product

--- models/Product.py
+++ models/Product.py
@@ -179,3 +179,18 @@ class ProductModel:
             }
         ]
 
+
+    def setProduct(self, new_data):
+        """Add a new product to the inventory"""
+        # Find the highest ID and increment it
+        new_id = max([p['id'] for p in self.products]) + 1 if self.products else 1
+        new_product = {
+            'id': new_id,
+            'nama': new_data['nama'],
+            'origin': new_data['origin'],
+            'roast': new_data['roast'],
+            'harga': int(new_data['harga']),
+            'stok': int(new_data['stok'])
+        }
+        self.products.append(new_product)
+        return new_product
